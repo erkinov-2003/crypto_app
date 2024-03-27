8959e1a5d999295181d0ca8d7c5729267d650d74
erkinov-2003 <azizerkinov2001@gmail.com>
2024-03-27 11:20:38 +0500

    added color

--- lib/src/core/constants/app_colors.dart
+++ lib/src/core/constants/app_colors.dart
@@ -4,4 +4,12 @@ class AppColors {
   static const whiteColor = Colors.white;
   static const blackColor = Colors.black;
   static const yellowColor = Colors.yellow;
+  static const buttonColor = Color(0xFF202832);
+
+  static const buttonShadowColor1 = Color(0xFF8AD4EC);
+  static const buttonShadowColor2 = Color(0xFFEF96FF);
+  static const buttonShadowColor3 = Color(0xFFFF56A9);
+  static const buttonShadowColor4 = Color(0xFFFFAA6C);
+
+  static const backgroundColor = Color(0xFF080A0C);
 }
--- lib/src/core/constants/app_images.dart
+++ lib/src/core/constants/app_images.dart
@@ -1,5 +1,9 @@
 class AppImages {
-  static const onboardingImg1 = "";
-  static const onboardingImg2 = "";
-  static const onboardingImg3 = "";
+  static const onboardImg1 = "assets/images/onboard_img1.svg";
+  static const onboardImg2 = "assets/images/onboard_img2.svg";
+  static const onboardImg3 = "assets/images/onboard_img3.svg";
+
+  static const onboardingImg1 = "assets/images/onboarding_img1.svg";
+  static const onboardingImg2 = "assets/images/onboarding_img2.svg";
+  static const onboardingImg3 = "assets/images/onboarding_img3.svg";
 }
